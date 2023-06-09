dd5844e511a8ebce08e9fefac03a8249fbdcb807
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2023-06-09 13:59:39 -0300

    feat(DoubleVector): add basic class definitions

---
+++ double_vector.h
@@ -0,0 +1,24 @@
+#include<vector>
+
+class DoubleVector
+{
+private:
+    int capacity;
+    int size;
+    int head;
+    int tail;
+    std::vector<int> list;
+
+public:
+    DoubleVector(int capacity);
+    ~DoubleVector();
+
+    void push_back(int n);
+    void push_front(int n);
+
+    int pop_back();
+    int pop_front();
+
+    void right_shift(int n);
+    void left_shift(int n);
+};
