efe3d3777013ff2343a0f8907285ae48dc8c3b04
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2023-06-09 14:14:17 -0300

    feat(DoubleVector): left and right shift implementation

--- double_vector.cpp
+++ double_vector.cpp
@@ -1,3 +1,5 @@
+#include<algorithm>
+
 #include"double_vector.h"
 
 using namespace std;
@@ -12,4 +14,20 @@ DoubleVector::DoubleVector(int capacity) : capacity(capacity), size(0)
 
 DoubleVector::~DoubleVector()
 {
+}
+
+void DoubleVector::right_shift(int n) {
+    rotate(list.rbegin(), list.rbegin() + n, list.rend());
+    head += n;
+    tail += n;
+
+    return;
+}
+
+void DoubleVector::left_shift(int n) {
+    rotate(list.begin(), list.begin() + n, list.end());
+    head -= n;
+    tail -= n;
+
+    return;
 }
